7b3779a5d1ba24a2807264b354b6baa4883b05a2
liuyoung0 <jdbbjd@icloud.com>
2020-03-17 16:58:18 +0800

    FaceUnity Nama SDK v6.1

---
+++ LiveStreamingCapture/src/com/netease/livestreamingcapture/CrashApplication.java
@@ -0,0 +1,23 @@
+package com.netease.livestreamingcapture;
+
+
+import android.app.Application;
+
+public class CrashApplication extends Application {
+
+    private static CrashApplication crashApplication;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        crashApplication = this;
+
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+
+    public static CrashApplication getInstance() {
+        return crashApplication;
+    }
+}
+
